679683904cb86f42df91fc316bf471a00ccffc84
MIBlue119 <miblue119@gmail.com>
2022-04-02 18:29:57 +0800

    Feat: append the random number and revise the type of guess

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -5,6 +5,8 @@ use std::io ;
 //The `Rng` trait defines the methods
 use rand::Rng;
 
+//The `Ordering` type is another enum and has the variants `Less` `Greater` and `Equal`
+use std::cmp::Ordering;
 
 /* 
   main() is the entry point to the program
@@ -31,6 +33,19 @@ fn main() {
         .read_line(&mut guess) 
         // `&` indicates that this argument is a reference, we need add `mut` to make this argument mutable, we use reference to avoid copy data
         .expect("Failed to read line");
+    
+    //Rust allow us to `shadow` the previous value of guess with a new one
+    //The `trim` method on a `String` instance will eliminate any whitespace at the begin and end
+    //The `parse` method parses a string into come kind of number. 
+    // The colon `:` after guess tell rust we'll annotate the variable's type 
+    // `u32` is an usigned 32-bit integer
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
     // {} is placehoder to fill the variable value
     println!("You guessed:{}",guess);
+
+    match guess.cmp(&secret_number){
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"), 
+            Ordering::Equal => println!("You win!")
+    }
 }
