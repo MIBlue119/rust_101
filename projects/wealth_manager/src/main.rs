166af53e000ab48e0e7a9e722c95899c96dfac53
weiren Lan <weirenlan@weirendeMacBook-Pro.local>
2022-04-05 15:14:54 +0800

    Feat: add the order functions

--- projects/wealth_manager/src/main.rs
+++ projects/wealth_manager/src/main.rs
@@ -1,11 +1,14 @@
 mod stocks;
+use stocks::{open_order,close_order};
+use stocks::structs::order::Order;
+use stocks::enums::order_types::OrderType;
 use stocks::structs::stock::Stock;
 
 //use serde_json::{json,Value};
 
 fn main() {
-    let stock: Stock = Stock::new("MonolithAi", 45.3);
-    println!("Here is the stock name:{}",stock.name);
+    // let stock: Stock = Stock::new("MonolithAi", 45.3);
+    // println!("Here is the stock name:{}",stock.name);
 //     let stock: Value =json!({
 //         "name":"Weiren",
 //         "price": 43.17,
@@ -15,5 +18,24 @@ fn main() {
 //     println!("Hello, world!");
 //     println!("{}",stock.to_string());
 // }
+
+println!("Hello stocks!");
+let mut new_order:Order = open_order(20, OrderType::Long, "bumper", 56.8, None, None);
+
+println!("The current price is:{}", &new_order.current_value());
+println!("The current profit is:{}", &new_order.current_profit());
+
+new_order.stock.update_price(43.1);
+
+println!("The current price is:{}", &new_order.current_value());
+println!("The current profit is:{}", &new_order.current_profit());
+
+new_order.stock.update_price(82.7);
+println!("The current price is:{}", &new_order.current_value());
+println!("The current profit is:{}", &new_order.current_profit());
+
+
+let profit: f32 = close_order(new_order);
+println!("We made {} profit", profit);
 
 }
--- projects/wealth_manager/src/stocks/mod.rs
+++ projects/wealth_manager/src/stocks/mod.rs
@@ -1,1 +1,27 @@
 pub mod structs;
+pub mod enums;
+use structs::stock::Stock;
+use structs::order::Order;
+use enums::order_types::OrderType;
+
+
+
+pub fn close_order(order:Order)->f32{
+    println!("order for {} is being close", &order.stock.name);
+    return order.current_profit()
+}
+
+pub fn open_order(number:i32, order_type:OrderType,stock_name:&str, open_price:f32, stop_loss:Option<f32>,take_profit:Option<f32>)-> Order {
+    println!("order for {} is being made", &stock_name);
+    let mut stock: Stock = Stock::new(stock_name, open_price);
+    match stop_loss{
+        Some(value)=>stock=stock.with_stop_loss(value),
+        None=>{}
+    }
+
+    match take_profit{
+        Some(value)=> stock=stock.with_take_profit(value),
+        None=>{}
+    }
+    return Order::new(stock, number,order_type)
+}
